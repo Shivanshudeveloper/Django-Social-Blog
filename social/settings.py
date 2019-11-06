d97fd9d44619871ea099769bb2e65d0173774af8
Shivanshu Gupta <40379768+Shivanshudeveloper@users.noreply.github.com>
2019-11-06 05:14:21 -0800

    deployemnt changing

--- social/settings.py
+++ social/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+
+# For Heroku Deployment
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
